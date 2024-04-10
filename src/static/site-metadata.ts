0a7f878a81c09f9d625dbd17ee534f93bf191aae
Vensent.Wang <kobeangeles8@gmail.com>
2024-04-11 00:21:37 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,7 +11,7 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Vensent.Wang's Workouts Page',
+  siteTitle: 'Vensent.Wang Workouts Page',
   siteUrl: 'https://ben29.xyz',
   logo: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2Fc7c75f8b-4dad-4b59-8bcf-b2244ad4815e%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1715356637&t=40a20e3c970a767af9aef86e07279c2a',
   description: 'Personal site and blog',
